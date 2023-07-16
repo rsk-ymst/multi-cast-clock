34beb763ddd836193d2114456642cf97ac36bf05
rsk-ymst <jey.togo@gmail.com>
2023-07-16 12:35:32 +0900

    [add] 乱数生成によるスレッドsleep関数実装

--- src/clock.rs
+++ src/clock.rs
@@ -1,4 +1,10 @@
-use std::{time::Duration, sync::{Mutex, Arc}, thread::{self, JoinHandle}};
+use std::{
+    sync::{Arc, Mutex},
+    thread::{self, JoinHandle},
+    time::Duration,
+};
+
+use rand::Rng;
 
 pub const TICK_INTERVAL: Duration = Duration::from_millis(100);
 
@@ -29,4 +35,10 @@ pub fn start_clock_tick(shared_value: &Arc<Mutex<LogicClock>>) -> JoinHandle<()>
             // println!("clock: {:.1}", clock.lock().unwrap().clock);
         }
     })
+}
+
+/* 1 ~ 3 tick分のスリープをランダムで生成する。常に等間隔でスリープさせると、レシーバ間のタイムスタンプにばらつきが生じないため。  */
+pub fn sleep_random_interval() {
+    let random_num: u64 = rand::thread_rng().gen_range(1..=3);
+    thread::sleep(Duration::from_millis(random_num * 100))
 }
