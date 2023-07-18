e0d2db9426068e44bfcaec6fccdb8ae849df40e5
rsk-ymst <jey.togo@gmail.com>
2023-07-18 19:07:03 +0900

    [add] Lamportのクロック調整機能追加

--- src/clock.rs
+++ src/clock.rs
@@ -38,7 +38,25 @@ pub fn start_clock_tick(shared_value: &Arc<Mutex<LogicClock>>) -> JoinHandle<()>
 }
 
 /* 1 ~ 3 tick分のスリープをランダムで生成する。常に等間隔でスリープさせると、レシーバ間のタイムスタンプにばらつきが生じないため。  */
-pub fn sleep_random_interval() {
-    let random_num: u64 = rand::thread_rng().gen_range(1..=3);
-    thread::sleep(Duration::from_millis(random_num * 100))
+pub fn sleep_random_interval(receiver_id: usize) {
+    let random_num: u64 = if receiver_id == 2 {
+        rand::thread_rng().gen_range(3..=6)
+    } else {
+        rand::thread_rng().gen_range(2..=3)
+    };
+    thread::sleep(Duration::from_millis(100))
+}
+
+pub fn adjust_time_clock(current_time: &Arc<Mutex<LogicClock>>, received_time: f64) {
+    /* もし未来からのメッセージが来ていたら、時刻を調整 */
+    match current_time.lock() {
+        Ok(mut current) => {
+            if received_time > current.clock {
+                current.clock = received_time + 1.0;
+            }
+        }
+        Err(_) => panic!(),
+    };
+
+    // drop(current); // Mutexロック解除
 }
